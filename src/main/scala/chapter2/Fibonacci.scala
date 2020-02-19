cab1c9ef599c0f1ce426068551926824593f704d
lewis.coleman <27Cobalt>
2020-02-19 12:50:13 +0000

    chap2 fib

--- src/main/scala/chapter2/Fibonacci.scala
+++ src/main/scala/chapter2/Fibonacci.scala
@@ -2,6 +2,15 @@ package chapter2
 
 object Fibonacci {
 
+  def fib(b: Int): Int = {
 
+    @scala.annotation.tailrec
+    def loop(b: Int, current: Int, prev: Int): Int = {
+      if (b == 1) prev
+      else loop(b - 1, current + prev, current)
+    }
+    loop(b, 1, 0)
+  }
 
+  // }
 }
--- src/test/scala/chapter2/FibonacciSpec.scala
+++ src/test/scala/chapter2/FibonacciSpec.scala
@@ -1,8 +1,29 @@
 package chapter2
 
-import org.scalatest.matchers.must.Matchers
-import org.scalatest.propspec.AnyPropSpecLike
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
 
-class FibonacciSpec extends AnyPropSpecLike with Matchers {
+class FibonacciSpec extends AnyFreeSpec with Matchers {
+
+  "Fib" - {
+    "should return 0 when 1 is inputted to fib" in {
+      Fibonacci.fib(1) shouldBe 0
+    }
+    "should return 1 when 2 is inputted into fib" in {
+      Fibonacci.fib(2) shouldBe 1
+    }
+
+    "should return 1 when 3 is inputted into fib" in {
+      Fibonacci.fib(3) shouldBe 1
+    }
+
+    "should return 13 when 8 is inputted into fib" in {
+      Fibonacci.fib(8) shouldBe 13
+    }
+
+    "should return 144 when 13 is inputted into fib" in {
+      Fibonacci.fib(13) shouldBe 144
+    }
+  }
 
 }
