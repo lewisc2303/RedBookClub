7c97b446801578cc9cb2c36e6981101141655ae3
lewisc2303 <lewiscoleman1995@hotmail.com>
2020-03-02 17:22:25 +0000

    chp3 more

--- src/main/scala/chapter3/myList.scala
+++ src/main/scala/chapter3/myList.scala
@@ -36,8 +36,18 @@ object MyList {
 
   def drop[A](list: MyList[A], n: Int): MyList[A] = list match {
     case Nil => Nil
-    case Cons(x, tail) if n == 0 => Cons(x, tail)
+    case list if n == 0 => list
     case Cons(x, tail) => drop(tail, n - 1)
+  }
+
+  def take[A](list: MyList[A], n: Int): MyList[A] = {
+    def loop[A](list: MyList[A], newList: MyList[A], n: Int): MyList[A] =
+      list match {
+        case Nil => Nil
+        case _ if n == 0 => newList
+        case Cons(x, tail) => loop(tail, append(newList, MyList(x)), n - 1)
+      }
+    loop(list, Nil, n)
   }
 
   def dropWhile[A](list: MyList[A], predicate: A => Boolean): MyList[A] = list match {
@@ -112,15 +122,60 @@ object MyList {
     case Cons(x, tail) => Cons(f(x), map(tail, f))
   }
 
+  def filter[A, B](list: MyList[A], predicate: A => Boolean): MyList[A] = list match {
+    case Nil => Nil
+    case Cons(x, tail) if predicate(x) => Cons(x, filter(tail, predicate))
+    case Cons(_, tail) => filter(tail, predicate)
+  }
+
+  def flatMap[A, B](list: MyList[A])(f: A => MyList[B]): MyList[B] = list match {
+    case Nil => Nil
+    case Cons(x, xs) => append(f(x), flatMap(xs)(f))
+  }
+
+  //implement flatmap using filter?
+  def flatMapFilter[A, B](list: MyList[A], predicate: A => Boolean): MyList[A] = ???
+
+  def sumLists(list1: MyList[Int], list2: MyList[Int]): MyList[Int] = (list1, list2) match {
+    case (list1, list2) if length(list1) != length(list2) => throw new Error
+    case (Nil, Nil) => Nil
+    case (Cons(x, xs), Cons(y, ys)) => Cons(x + y, sumLists(xs, ys))
+  }
+
+  def zipWith[A, B, C](list1: MyList[A], list2: MyList[B])(f: (A, B) => C ): MyList[C] = (list1, list2) match {
+    case (list1, list2) if length(list1) != length(list2) => throw new Error
+    case (Nil,  Nil) => Nil
+    case (Cons(x, xs), Cons(y, ys)) => Cons(f(x,y), zipWith(xs, ys)(f))
+  }
+
+  def createLists[A](list: MyList[A], n: Int): MyList[Int] = list match {
+    case Nil => Cons(0, Nil)
+    case Cons(_, tail) => Cons(n, createLists(tail, n - 1))
+  }
+
+  def hasSuqsequence[A](sup: MyList[A], sub: MyList[A]): MyList[MyList[A]] = {
+    val listLength = length(sup)
+
+    def loop[A](sup: MyList[A], n: Int, lists: MyList[MyList[A]]): MyList[MyList[A]] = sup match {
+      case _ if n == 0 => lists
+      case Nil => Nil
+      case Cons(_, xs) => loop(xs, n - 1, append(lists, map(reverse(createLists(sup, n)), (int: Int) =>  drop(sup, int))))
+    }
+    filter(loop(sup, listLength, Nil), (x: MyList[A]) => x == sub)
+  }
 
 }
 
 object main extends App {
   import MyList._
 
-  println(reverse(MyList(1,2,3,4)))
+//  println(take(MyList(1,2,3,4), 4))
+//
+//  println(flattenList(MyList(MyList(1,2,3), MyList(4,5,6))))
 
-  println(flattenList(MyList(MyList(1,2,3), MyList(4,5,6))))
+  println(createLists(MyList(1,2,3,4), 4))
+
+  println(hasSuqsequence(MyList(1,2,3,4), MyList(1,2)))
 }
 
 
